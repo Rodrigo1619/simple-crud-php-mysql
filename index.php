e0dace765b48a91cbcdc3ca1ed338152bf1ab356
Rodrigo1619 <00067920@uca.edu.sv>
2024-12-23 16:54:56 -0600

    simple form and table

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud php-mysql</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+</head>
+
+<body>
+    <h1 class="text-center p-3">ola mundo</h1>
+    <div class="container-fluid row">
+        <form class="col-4 p-3">
+            <h3 class="text-center text-secondary">Registro de personas</h3>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nombre de la persona</label>
+                <input type="text" class="form-control" name="nombre">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apellido de la persona</label>
+                <input type="text" class="form-control" name="apellido">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">DNI de la persona</label>
+                <input type="text" class="form-control" name="dni">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
+                <input type="date" class="form-control" name="fecha">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Correo</label>
+                <input type="text" class="form-control" name="correo">
+            </div>
+
+            <button type="submit" class="btn btn-primary" name="btnregistrar" value="ok">Registrar</button>
+        </form>
+
+        <!-- Tabla -->
+        <div class="col-8 p-4">
+            <table class="table">
+                <thead class="bg-info">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">NOMBRE</th>
+                        <th scope="col">APELLIDOS</th>
+                        <th scope="col">DNI</th>
+                        <th scope="col">FECHA DE NAC</th>
+                        <th scope="col">CORREO</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>
+                            <a href="" class="btn btn-small btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="" class="btn btn-small btn-danger"><i class="fa-solid fa-trash"></i></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+
+    <!-- Para bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
